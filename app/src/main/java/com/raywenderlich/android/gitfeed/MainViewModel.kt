080462b0f4921bac346145e7c819d89c2e312191
Khin <khinthirisoe.mdy@gmail.com>
2019-08-14 22:19:57 +0700

    fetch top response

--- app/src/main/java/com/raywenderlich/android/gitfeed/MainViewModel.kt
+++ app/src/main/java/com/raywenderlich/android/gitfeed/MainViewModel.kt
@@ -42,83 +42,92 @@ import io.reactivex.schedulers.Schedulers
 
 class MainViewModel : ViewModel() {
 
-  val eventLiveData = MutableLiveData<List<Event>>()
+    val eventLiveData = MutableLiveData<List<Event>>()
 
-  private var lastModified: String? = null
+    private var lastModified: String? = null
 
-  var events = mutableListOf<Event>()
+    var events = mutableListOf<Event>()
 
-  private val gitHubApi by lazy {
-    GitHubApi.create()
-  }
-
-  private val disposables = CompositeDisposable()
-
-  fun fetchEvents(repo: String) {
-
-    eventLiveData.value = EventsStore.readEvents()
-
-    lastModified = EventsStore.readLastModified()
-
-    val apiResponse = gitHubApi.fetchEvents(
-      repo, lastModified?.trim() ?: ""
-    ).share()
-    apiResponse
-      .filter { response ->
-        (200..300).contains(response.code())
-      }
-      .map { response ->
-        response.body()
-      }
-      .filter { objects ->
-        objects.isNotEmpty()
-      }
-      .map { objects ->
-        objects.mapNotNull { Event.fromAnyDict(it) }
-      }
-      .subscribeOn(Schedulers.io())
-      .observeOn(AndroidSchedulers.mainThread())
-      .subscribeBy(
-        onNext = { events -> processEvents(events) },
-        onError = { error -> println("Error ::: ${error.message}")}
-      ).addTo(disposables)
-
-    apiResponse
-      .filter { response ->
-         (200 until 400).contains(response.code())
-      }
-      .flatMap { response ->
-        val value = response.headers().get("Last-modified")
-        if (value == null){
-          Observable.empty()
-        } else {
-          Observable.just(value)
-        }
-      }
-      .subscribeOn(Schedulers.io())
-      .observeOn(AndroidSchedulers.mainThread())
-      .subscribeBy(
-        onNext = { lastModified -> EventsStore.saveLastModified(lastModified) },
-        onError = { error -> println("Last Modified Error ::: ${error.message}")}
-      ).addTo(disposables)
-  }
-
-  override fun onCleared() {
-    super.onCleared()
-    disposables.dispose()
-  }
-
-  private fun processEvents(newEvents: List<Event>) {
-    var updatedEvents = newEvents + events
-
-    if (updatedEvents.size > 50) {
-      updatedEvents = updatedEvents.slice(0 until 50)
+    private val gitHubApi by lazy {
+        GitHubApi.create()
     }
 
-    events = updatedEvents.toMutableList()
+    private val disposables = CompositeDisposable()
 
-    eventLiveData.value = events
+    fun fetchEvents() {
 
-    EventsStore.saveEvents(events)
-  }
+        eventLiveData.value = EventsStore.readEvents()
+
+        lastModified = EventsStore.readLastModified()
+
+        val topResponse = gitHubApi.fetchTopKotlinRepos()
+        topResponse
+            .flatMap { response: TopResponse ->
+                if (response.items == null) {
+                    Observable.empty()
+                } else {
+                    Observable.fromIterable(response.items)
+                        .map { it["full_name"] as String }
+                }
+            }
+            .flatMap {
+                gitHubApi.fetchEvents(it)
+            }
+            .filter { response ->
+                (200..300).contains(response.code())
+            }
+            .map { response ->
+                response.body()
+            }
+            .filter { objects ->
+                objects.isNotEmpty()
+            }
+            .map { objects ->
+                objects.mapNotNull { Event.fromAnyDict(it) }
+            }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onNext = { events -> processEvents(events) },
+                onError = { error -> println("Error ::: ${error.message}") }
+            ).addTo(disposables)
+
+//    apiResponse
+//      .filter { response ->
+//         (200 until 400).contains(response.code())
+//      }
+//      .flatMap { response ->
+//        val value = response.headers().get("Last-modified")
+//        if (value == null){
+//          Observable.empty()
+//        } else {
+//          Observable.just(value)
+//        }
+//      }
+//      .subscribeOn(Schedulers.io())
+//      .observeOn(AndroidSchedulers.mainThread())
+//      .subscribeBy(
+//        onNext = { lastModified -> EventsStore.saveLastModified(lastModified) },
+//        onError = { error -> println("Last Modified Error ::: ${error.message}")}
+//      ).addTo(disposables)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.dispose()
+    }
+
+    private fun processEvents(newEvents: List<Event>) {
+        var updatedEvents = newEvents + events
+
+        if (updatedEvents.size > 50) {
+            updatedEvents = updatedEvents.slice(0 until 50)
+        }
+
+        events = updatedEvents.toMutableList()
+
+        eventLiveData.value = events
+
+        EventsStore.saveEvents(events)
+    }
 }
